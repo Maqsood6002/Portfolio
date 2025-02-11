f33856012a8ab770bf79b78f0f02d4ce44fe8e0c
Maqsood <139763980+Maqsood6002@users.noreply.github.com>
2025-02-11 17:59:56 +0530

    First Commit

---
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-scroll";
+
+const AboutMeText = () => {
+  return (
+    <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
+      <h2 className="text-6xl text-cyan mb-10">About Me</h2>
+      <p>
+        I’m Maqsood, I’m a passionate Web Developer with expertise in the Full stack, specializing in building scalable and user-friendly applications. With a strong background in front-end and back-end development, I enjoy turning ideas into functional, high-performance web solutions. Beyond coding, I’m always eager to learn, explore new technologies, and contribute to impactful projects. My goal is to build applications that solve real-world problems while continuously improving as a developer.
+      </p>
+      <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
+        <Link
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-120}
+          to="projects"
+          className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+        >
+          My Projects
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default AboutMeText;
